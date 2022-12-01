e54dfed6b9cedec34bb6ddd4135f266f44001eee
mica4003 <mica4003@colorado.edu>
2022-11-30 21:32:56 -0700

    game.html

---
+++ javascript.js
@@ -0,0 +1,43 @@
+// GAME
+let playerScore = 0;
+let computerScore = 0;
+let roundWinner = '';
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    roundWinner = 'tie';
+  }
+  if (
+    (playerSelection === 'ROCK' && computerSelection === 'SCISSORS') ||
+    (playerSelection === 'SCISSORS' && computerSelection === 'PAPER') ||
+    (playerSelection === 'PAPER' && computerSelection === 'ROCK')
+  ) {
+    playerScore++;
+    roundWinner = 'player';
+  }
+  if (
+    (computerSelection === 'ROCK' && playerSelection === 'SCISSORS') ||
+    (computerSelection === 'SCISSORS' && playerSelection === 'PAPER') ||
+    (computerSelection === 'PAPER' && playerSelection === 'ROCK')
+  ) {
+    computerScore++;
+    roundWinner = 'computer';
+  }
+  updateScoreMessage(roundWinner, playerSelection, computerSelection)
+}
+
+function getRandomChoice() {
+  let randomNumber = Math.floor(Math.random() * 3)
+  switch (randomNumber) {
+    case 0:
+      return 'ROCK';
+    case 1:
+      return 'PAPER';
+    case 2:
+      return 'SCISSORS';
+  }
+}
+
+function isGameOver() {
+  return playerScore === 5 || computerScore === 5;
+}
